cdb9773bca7477c495df0713c2355b81639ee920
Ankush263 <ankushbanik263@gmail.com>
2026-06-02 22:58:00 +0530

    Added db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from app.db.session import get_db
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return { "message": "Hello Ankush" }
+
+@app.get("/db-health")
+def healthcheck(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT current_database();"))
+    database_name = result.scalar()
+
+    return {
+        "status": "connected",
+        "database": database_name
+    }
